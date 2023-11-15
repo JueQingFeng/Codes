ed552abcd08c4477ab0d286b4efed206889ba1d6
JueQingFeng <CL3373957309@outlook.com>
2023-11-16 07:16:46 +0800

    最终版

--- 期望/P5011 水の造题.cpp
+++ 期望/P5011 水の造题.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 
 typedef long long ll;
-const ll mod = 19451001;
+const ll mod = 19491001;
 
 ll N, K;
 ll a[1100000];
@@ -17,12 +17,25 @@ ll QuickPower(ll ttt, ll k) {
     return result;
 }
 
+char ch[1100000];
 ll answer, inv;
 
 int main() {
-    scanf("%lld%lld", &N, &K);
-    for (int i = 1; i <= K; ++ i)
-        scanf("%lld", &a[i]);
+    scanf("%s%lld", ch + 1, &K);
+    int len = strlen(ch + 1);
+    for (int i = 1; i <= len; ++ i) {
+        N = (N * 10 % mod + ch[i] - '0') % mod;
+    }
     inv = QuickPower(K, mod - 2);
-    
+    for (int i = 1; i <= K; ++ i) {
+        scanf("%lld", &a[i]);
+        answer = (answer + a[i] * inv % mod) % mod;
+    }
+    a[0] = a[K];
+    for (int i = 1; i <= K; ++ i) {
+        answer = (answer + a[i] * (N - 1) % mod * inv % mod) % mod;
+        answer = (answer + (N - 1) * inv % mod * inv % mod * ((a[i] + a[i - 1]) % mod) % mod) % mod;
+    }
+    printf("%lld\n", answer);
+    return 0;
 }
